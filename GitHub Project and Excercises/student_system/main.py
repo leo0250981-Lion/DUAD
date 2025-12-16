58fb9e105ce70924cedf7fa8806b838c74cbd8a0
leo0250981-Lion <leo0250981@icloud.com>
2025-12-16 11:07:49 -0500

    Update main.py
    Refactor menu to remove global variables and use function parameters

--- GitHub Project and Excercises/student_system/main.py
+++ GitHub Project and Excercises/student_system/main.py
@@ -1,7 +1,18 @@
 from menu import show_menu
 
 def main():
-    show_menu()
+    while True:
+        option = show_menu()
+
+        if option == "1":
+            print("Add a student")
+        elif option == "2":
+            print("Show Enlisted Students")
+        elif option == "3":
+            print("Exiting the program...")
+            break
+        else:
+            print("Invalid option, please try again.")
 
 if __name__ == "__main__":
     main()
