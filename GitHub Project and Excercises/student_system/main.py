a199e13546e3c0811f1b347574ccc61f53c1eff6
leo0250981-Lion <leo0250981@icloud.com>
2025-12-16 11:14:18 -0500

    Update main.py
    Changes to main.py in the student_system project

--- GitHub Project and Excercises/student_system/main.py
+++ GitHub Project and Excercises/student_system/main.py
@@ -5,14 +5,26 @@ def main():
         option = show_menu()
 
         if option == "1":
-            print("Add a student")
+            print("Add new student")
         elif option == "2":
-            print("Show Enlisted Students")
+            print("Display all students")
         elif option == "3":
+            print("Top 3 students by average")
+        elif option == "4":
+            print("Global average score")
+        elif option == "5":
+            print("Show failing students")
+        elif option == "6":
+            print("Export students to CSV")
+        elif option == "7":
+            print("Import students from CSV")
+        elif option == "8":
+            print("Delete a student")
+        elif option == "9":
             print("Exiting the program...")
             break
         else:
-            print("Invalid option, please try again.")
+            print("Invalid option. Please try again.")
 
 if __name__ == "__main__":
     main()
