2459b13782df3253befa4f6f22253bf46f90935b
leo0250981-Lion <leo0250981@icloud.com>
2025-12-16 11:13:06 -0500

    Update menu.py
    Changes to the menu.py in the student_system project

--- GitHub Project and Excercises/student_system/menu.py
+++ GitHub Project and Excercises/student_system/menu.py
@@ -1,8 +1,30 @@
-def show_menu():
-    print("\n--- STUDENT SYSTEM MENU ---")
-    print("1. Add a student")
-    print("2. Show enlisted students")
-    print("3. Exit")
+from menu import show_menu
 
-    choice = input("Select an option (1-3): ")
-    return choice
+def main():
+    while True:
+        option = show_menu()
+
+        if option == "1":
+            print("Add new student")
+        elif option == "2":
+            print("Display all students")
+        elif option == "3":
+            print("Top 3 students by average")
+        elif option == "4":
+            print("Global average score")
+        elif option == "5":
+            print("Show failing students")
+        elif option == "6":
+            print("Export students to CSV")
+        elif option == "7":
+            print("Import students from CSV")
+        elif option == "8":
+            print("Delete a student")
+        elif option == "9":
+            print("Exiting the program...")
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+if __name__ == "__main__":
+    main()
