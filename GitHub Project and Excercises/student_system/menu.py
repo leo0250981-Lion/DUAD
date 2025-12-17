0347cf2e787df7c7e52985ab466a2643c5445fe6
leo0250981-Lion <leo0250981@icloud.com>
2025-12-16 19:33:03 -0500

    Update menu.py

--- GitHub Project and Excercises/student_system/menu.py
+++ GitHub Project and Excercises/student_system/menu.py
@@ -1,8 +1,14 @@
 def show_menu():
-    print("\n--- STUDENT SYSTEM MENU ---")
-    print("1. Add a student")
-    print("2. Show enlisted students")
-    print("3. Exit")
+    print("\n=== STUDENT MANAGEMENT SYSTEM ===")
+    print("1. Add new student")
+    print("2. Display all students")
+    print("3. Top 3 students by average")
+    print("4. Global average score")
+    print("5. Show failing students")
+    print("6. Export students to CSV")
+    print("7. Import students from CSV")
+    print("8. Delete a student")
+    print("9. Exit")
 
-    choice = input("Select an option (1-3): ")
-    return choice
+    option = input("Select an option (1-9): ")
+    return option
